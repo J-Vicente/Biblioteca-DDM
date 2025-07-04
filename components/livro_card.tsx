58931da210c6c6a4b89002f668c6dae4b5d91981
J-Vicente <joaovicent2713@gmail.com>
2025-07-04 11:05:10 -0300

    estilização de cards livros

--- components/livro_card.tsx
+++ components/livro_card.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 interface CardLivroProps{
     titulo? : string;
@@ -10,12 +10,46 @@ interface CardLivroProps{
 
 const CardLivro = (props: CardLivroProps) => {
     return(
-        <View>  
-            <Text>{props.titulo}</Text>
-            <Text>{props.autor}</Text>
-            <Text>{props.categoria}</Text>
+        <View style={styles.card}>  
+            <Text style={styles.titulo}>{props.titulo}</Text>
+            <Text style={styles.text}>{props.autor}</Text>
+            <Text style={styles.text}>{props.categoria}</Text>
         </View>
     );
 }  
 
 export default CardLivro;
+
+const styles = StyleSheet.create({
+  card:{
+    width: 170,
+    height: "100%",
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    flex: 1,
+    alignItems: "center",
+    borderStyle:"solid",
+    borderColor:"black",
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 50,
+    marginBottom: 30,
+    backgroundColor:"#053ADE",
+    
+  },
+  text:{
+    fontSize:15,
+    color:"white",
+    marginBottom:10,
+  },
+  titulo:{
+    color:"white",
+    textAlign:"center",
+    width:"100%",
+    fontWeight:"bold",
+    marginBottom: 10,
+    fontSize: 15,
+  }
+ 
+});
